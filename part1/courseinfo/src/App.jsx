ef1398e0acd4fd2df84bce4a4e4a2a7d4ae79782
tonyweckstrom1987 <tony.weckstrom1987@gmail.com>
2026-07-10 09:01:52 +0300

    Add second course and split components into modules (part 2.1-2.5)

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,41 +1,44 @@
-const Header = ({ course }) => <h1>{course}</h1>
+import Header from './components/header'
 
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
+import Content from './components/Content'
 
-const Content = ({ parts }) => (
-  <div>
-    {parts.map((part) => (
-      <Part key={part.id} part={part} />
-    ))}
-  </div>
-)
+import Total from './components/Total'
 
-const Total = ({ parts }) => {
-  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
-  return <p>Number of exercises {total}</p>
-}
 
-const App = () => {
-  const course = {
+const courses = [
+  {
     name: 'Half Stack application development',
+    id: 1,
     parts: [
       { id: 1, name: 'Fundamentals of React', exercises: 10 },
       { id: 2, name: 'Using props to pass data', exercises: 7 },
       { id: 3, name: 'State of a component', exercises: 14 },
     ],
-  }
+  },
+  {
+    name: 'Node.js',
+    id: 2,
+    parts: [
+      { id: 1, name: 'Routing', exercises: 3 },
+      { id: 2, name: 'Middlewares', exercises: 7 },
+    
+    ],
+  },
+]
 
+const App = () => {
   return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
-    </div>
-  )
+  <div>
+    {courses.map((course) => (
+      <div key={course.id}>
+        <Header course={course.name} />
+        <Content parts={course.parts} />
+        <Total parts={course.parts} />
+      </div>
+    ))}
+  </div>
+)
+
 }
 
 export default App
---
+++ part1/courseinfo/src/components/content.jsx
@@ -0,0 +1,9 @@
+import Part from './part'
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+export default Content
