8873a2da76eaa13977275ae562b5702a7b68ae58
tonyweckstrom1987 <tony.weckstrom1987@gmail.com>
2026-07-17 09:50:33 +0300

    Serve frontend production build from backend (part 3.11)

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -6,6 +6,7 @@ morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 app.post('/api/persons', (req, res) => {
   const body = req.body
 
